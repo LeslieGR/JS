e64a5029901a12d38960a208770e7f5443caab78
LG <leslie.garciardgz@gmail.com>
2023-03-27 23:13:07 -0600

    Se agregan y editan archivos de la carpeta ejercicio2

---
+++ JS-04/ejercicio2/js/main.js
@@ -0,0 +1,12 @@
+console.log("********** Ejercicio 2 **********")
+let inicio, x;
+for(inicio = 1; inicio <= 5; inicio++)
+{
+    let patron="";
+for(x = 1; x <= inicio; x++)
+{
+  patron +="* ";
+}
+console.log(patron);
+}
+
